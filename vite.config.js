335257b5aa32a14eed9f777df5e4d1ae74c2ffa6
Nisarg711 <161602810+Nisarg711@users.noreply.github.com>
2024-09-04 15:20:54 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base:"/Astral_Adventures",
+   base: '/Astral-Adventures/',
   plugins: [react()],
  
 });
